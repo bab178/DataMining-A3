a408747f124c926705cc90745dacfb2d8f2c1aa1
Blake Bordovsky <blakebordovsky@gmail.com>
2017-03-20 14:53:25 -0500

    Change solr to use angular ngsubmit Allow access control origin all

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,14 @@
 angular.module('DataMiningA3.controllers', [])
 .controller('DashCtrl', function ($scope, $state, $http, $rootScope) {
-    
+    $scope.q = "";
+    $scope.solr = function() {
+        jQuery.get({
+            type: 'GET',
+            url: 'http://52.35.144.231:8983/solr/lyrics/select?debug=query&wt=json&q=' + $scope.q,
+            contentType: 'json',
+            dataType: "json"
+        }).done(function (data) {
+            alert("Data Loaded: " + data);
+        });
+    }
 });
--- server.js
+++ server.js
@@ -11,6 +11,24 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.listen(port);
 console.log("Server is now listening on port: " + port);
 exports = module.exports = app;
